f26230547260ffe72bd96778fb943161c67dddfe
Shtoner <zstoner57@gmail.com>
2022-08-13 13:56:28 -0500

    maegins

--- src/App.js
+++ src/App.js
@@ -41,7 +41,7 @@ const About = () => (
     <img src={logo} className="App-logo" alt="logo" />
 
     <h1>About Me</h1>
-    <p>Ipsum dolor dolorem consectetur est velit fugiat. Dolorem provident corporis fuga saepe distinctio ipsam? Et quos harum excepturi dolorum molestias?</p>
+    <p>I am a web developer who is ready to get to work building the best websites possible.</p>
     <p>Ipsum dolor dolorem consectetur est velit fugiat. Dolorem provident corporis fuga saepe distinctio ipsam? Et quos harum excepturi dolorum molestias?</p>
   </div>
 );
