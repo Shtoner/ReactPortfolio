f1746caf0eada3959d9438a9d9c796e891d13a87
Shtoner <zstoner57@gmail.com>
2022-11-28 18:26:21 -0600

    cleanded code

--- src/sendTransaction..js
+++ src/sendTransaction..js
@@ -12,28 +12,31 @@ import{utils} from 'ethers'
 import App from './chainstest'
 
 export function SendTransaction() {
- const [to, setTo] = React.useState('')
- const [debouncedTo] = useDebounce(to, 500)
 
- const [amount, setAmount] = React.useState('')
- const [debouncedAmount] = useDebounce(amount, 500)
 const chain= useNetwork()
-const { chains, error, pendingChainId, switchNetwork } =
+const { chains, error, isLoading, pendingChainId, switchNetwork } =
 useSwitchNetwork()
 console.log(chain.name)
- const { config } = usePrepareSendTransaction({
- request: {
- to: debouncedTo,
- value: debouncedAmount ? utils.parseEther(debouncedAmount) : undefined,
- },
- chainId: chain.name
- })
- const { data, sendTransaction } = useSendTransaction(config)
 
- const { isLoading, isSuccess } = useWaitForTransaction({
- hash: data?.hash,
- })
+function Forms(){
+    const [to, setTo] = React.useState('')
+    const [debouncedTo] = useDebounce(to, 500)
+   
+    const [amount, setAmount] = React.useState('')
+    const [debouncedAmount] = useDebounce(amount, 500)
 
+    const { config } = usePrepareSendTransaction({
+        request: {
+        to: debouncedTo,
+        value: debouncedAmount ? utils.parseEther(debouncedAmount) : undefined,
+        },
+        chainId: chain.name
+        })
+        const { data, sendTransaction } = useSendTransaction(config)
+       
+        const { isLoading1, isSuccess } = useWaitForTransaction({
+        hash: data?.hash,
+        })
  return (
     <>
     <h2>This form allows you to send transactions by specifying public address and wallet</h2>
@@ -68,7 +71,16 @@ console.log(chain.name)
  </div>
  </div>
  )}
- </form>
+ </form></>
+ )}
+
+ function ChainMap(){
+    const { chain } = useNetwork()
+const { chains, error, isLoading, pendingChainId, switchNetwork } =
+useSwitchNetwork()
+console.log(chain.name)
+    return(
+        <>
  {chain && <div>Connected to {chain.name}</div>}
 
 {chains.map((x) => (
@@ -84,6 +96,13 @@ onClick={() => switchNetwork?.(x.id)}
 
 <div>{error && error.message}</div>
 
- </>
+ </>)}
+ return(
+    <>
+    <Forms/>
+    <ChainMap/>
+    </>
+
  )
+
 }
